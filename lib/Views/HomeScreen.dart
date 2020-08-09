f61f39e9d44745fde47a9e9d8abe7693b85c31a3
SAMYAK99 <singhsamyak199@gmail.com>
2020-08-09 22:24:33 +0530

    Added Silver app bars with icon button

---
+++ lib/Views/HomeScreen.dart
@@ -0,0 +1,39 @@
+import 'package:facebookflutter/Widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:facebookflutter/config/palette.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            title: Text(
+              'facebook',
+              style: TextStyle(
+                color: Palette.facebookBlue,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+              ),
+            ),
+            actions: [
+              CircleButton(
+                  icon: Icons.search,
+                  iconSize: 30,
+                  onPressed: () => print('Search Button Tapped')),
+              CircleButton(
+                  icon: MdiIcons.facebookMessenger,
+                  iconSize: 30,
+                  onPressed: () => print('Massenger Button Tapped')),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
